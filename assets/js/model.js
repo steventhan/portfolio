cf2b9efdd9d47542d7fdc6ac008b22fbf333da3c
Steven Than <Steven@Stevens-iMac.wavecable.com>
2016-06-14 21:16:11 -0700

    Wrapped model js in iffe

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,54 +1,59 @@
-Project.all = [];
-function Project(projectData) {
-  for (property in projectData) {
-    this[property] = projectData[property];
+(function(module) {
+
+  Project.allProjects = [];
+  function Project(projectData) {
+    for (property in projectData) {
+      this[property] = projectData[property];
+    }
   }
-}
 
-Project.prototype.toHtml = function() {
-  var source = $('#project-template').html();
-  var template = Handlebars.compile(source);
-  var html = template(this);
-  return html;
-};
+  Project.prototype.toHtml = function() {
+    var source = $('#project-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(this);
+    return html;
+  };
 
 
-Project.loadAll = function (projectData) {
-  projectData.forEach(function(projectData) {
-    Project.all.push(new Project(projectData));
-  });
-};
+  Project.loadAll = function (projectData) {
+    Project.allProjects = projectData.map(function(projectData) {
+      return new Project(projectData);
+    });
+  };
 
-Project.fetchAll = function () {
-  if (localStorage.projectData) {
-    Project.loadAll(JSON.parse(localStorage.projectData));
-    appView.renderProjectPage();
-    // $.ajax({
-    //   url: 'assets/js/data/projects.json',
-    //   type: 'HEAD',
-    // }).done(function(data, message, xhr){
-    //   if (xhr.getResponseHeader('eTag') !== localStorage.eTag) {
-    //     localStorage.eTag = JSON.stringify(xhr.getResponseHeader('eTag'));
-    //     $.getJSON('assets/js/data/projects.json', function(data, message, xhr) {
-    //       localStorage.projectData = JSON.stringify(data);
-    //       Project.loadAll(JSON.parse(localStorage.projectData));
-    //       appView.renderProjectPage();
-    //     });
-    //   } else {
-    //     Project.loadAll(JSON.parse(localStorage.projectData));
-    //     appView.renderProjectPage();
-    //   }
-    // });
-  } else {
-    $.getJSON('assets/js/data/projects.json', function (data, message, xhr) {
-      localStorage.eTag = xhr.getResponseHeader('eTag');
-      localStorage.projectData = JSON.stringify(data);
+  Project.fetchAll = function () {
+    if (localStorage.projectData) {
       Project.loadAll(JSON.parse(localStorage.projectData));
       appView.renderProjectPage();
-    });
-  }
-};
+      // $.ajax({
+      //   url: 'assets/js/data/projects.json',
+      //   type: 'HEAD',
+      // }).done(function(data, message, xhr){
+      //   if (xhr.getResponseHeader('eTag') !== localStorage.eTag) {
+      //     localStorage.eTag = JSON.stringify(xhr.getResponseHeader('eTag'));
+      //     $.getJSON('assets/js/data/projects.json', function(data, message, xhr) {
+      //       localStorage.projectData = JSON.stringify(data);
+      //       Project.loadAll(JSON.parse(localStorage.projectData));
+      //       appView.renderProjectPage();
+      //     });
+      //   } else {
+      //     Project.loadAll(JSON.parse(localStorage.projectData));
+      //     appView.renderProjectPage();
+      //   }
+      // });
+    } else {
+      $.getJSON('assets/js/data/projects.json', function (data, message, xhr) {
+        localStorage.eTag = xhr.getResponseHeader('eTag');
+        localStorage.projectData = JSON.stringify(data);
+        Project.loadAll(JSON.parse(localStorage.projectData));
+        appView.renderProjectPage();
+      });
+    }
+  };
 
-Project.all.forEach(function(project){
-  $('#projects').append(project.toHtml());
-});
+  Project.all.forEach(function(project){
+    $('#projects').append(project.toHtml());
+  });
+
+  module.Project = Project;
+})(window);
